b8e009a028972674e9af8d3a45cc5e886851fb54
zopvan <zooganz1@gmail.com>
2026-03-31 17:46:00 +0700

    card monitor

--- src/components/dashboard.vue
+++ src/components/dashboard.vue
@@ -1,5 +1,22 @@
 <template>
-    <div data-section="titleWeb" class="relative h-screen w-full bg-gray-200 flex justify-center items-center  ">
-        <h1 class="text-4xl font-bold text-gray-800"">coolingy's</h1>
+    <div class="justify-items-center">
+        <div
+            data-section="titleWeb"
+            class="m-17.5 h-17.25 flex justify-center items-center"
+        >
+            <h1 class="text-[24px] font-bold text-gray-800">Coolingy's</h1>
+        </div>
+
+        <div class="bg-[#F2F2F2] p-1 w-87 h-44.75">
+            <div class="bg-[#D3D3D3] m-1 mt-1">
+                <div class="flex justify-between pl-1.5 pr-1.5 pt-2.5">
+                    <p>Coolingy's</p>
+                    <p>00:00:00</p>
+                </div>
+            </div>
+            <div class="text-[12px] flex justify-center items-center">
+                Realtime Monitor
+            </div>
+        </div>
     </div>
 </template>
